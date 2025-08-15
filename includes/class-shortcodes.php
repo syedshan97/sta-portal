7a30184742e68ca0d91ad4a5418dccd4768457a3
Syed Shan Shah <syedshan.biz@gmail.com>
2025-08-15 16:43:17 +0500

    Logout Functionionality via Shortcode [portal_logout_link text=Sign out]

--- includes/class-shortcodes.php
+++ includes/class-shortcodes.php
@@ -8,6 +8,8 @@ class STA_Portal_Shortcodes {
         add_shortcode( 'portal_signup_form', array( $this, 'signup_form_shortcode' ) );
         add_shortcode( 'portal_lost_password_form', array( $this, 'lost_password_form_shortcode' ) );
         add_shortcode( 'portal_reset_password_form', array( $this, 'reset_password_form_shortcode' ) );
+        add_shortcode('portal_logout_link', array($this, 'logout_link_shortcode'));
+
 
     }
 
@@ -36,5 +38,32 @@ class STA_Portal_Shortcodes {
     return ob_get_clean();
 
     }
+
+    public function logout_link_shortcode($atts) {
+    // Hide link for guests
+    if ( ! is_user_logged_in() ) return '';
+
+    $atts = shortcode_atts(array(
+        'text'     => 'Logout',
+        'class'    => 'sta-logout-link',
+        // Leave empty to use default success message redirect to /login/
+        'redirect' => '',
+    ), $atts, 'portal_logout_link');
+
+    // Default redirect: /login/?sta_success=You’ve been logged out.
+    $default_redirect = add_query_arg(
+        'sta_success',
+        urlencode("You’ve been logged out."),
+        site_url('/login/')
+    );
+
+    $target_redirect = $atts['redirect'] ? $atts['redirect'] : $default_redirect;
+
+    // Secure, nonce-protected WordPress logout URL
+    $url = wp_logout_url( $target_redirect );
+
+    return '<a href="'. esc_url($url) .'" class="'. esc_attr($atts['class']) .'">'. esc_html($atts['text']) .'</a>';
+}
+
 
 }
